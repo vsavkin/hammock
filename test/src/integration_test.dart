577f823194522542d9229065af5ca75ec8a568b3
Victor Savkin <vic.savkin@gmail.com>
2014-06-26 13:25:14 -0400

    Implement command/query separation
    It enables:
    1. Handling errors differently
    2. Handling commands and queries

--- test/src/integration_test.dart
+++ test/src/integration_test.dart
@@ -1,8 +1,24 @@
 part of hammock_test;
 
+class IntegrationPost {
+  int id;
+  String title;
+  String errors;
+}
+
 testIntegration() {
+  deserializePost(r) => new IntegrationPost()
+    ..id = r.id
+    ..title = r.content["title"]
+    ..errors = r.content["errors"];
+
+  serializePost(post) =>
+    resource("posts", post.id, {"id" : post.id, "title" : post.title});
+
+
+
   describe("Custom Document Formats", () {
-    it("can support jsonapi.org format", inject((HammockConfig config, MockHttpBackend hb, ResourceStore s){
+    it("can support jsonapi.org format", (HammockConfig config, MockHttpBackend hb, ResourceStore s){
       config.documentFormat = new JsonApiOrgFormat();
 
       hb.whenGET("/posts/123").respond({"posts" : [{"id" : 123, "title" : "title"}]});
@@ -12,38 +28,72 @@ testIntegration() {
 
       hb.expectPUT("/posts/123", '{"posts":[{"id":123,"title":"new"}]}');
       wait(s.save(resource("posts", 123, {"id" : 123, "title" : "new"})));
-    }));
+    });
   });
 
-  describe("Configuration", () {
-    it("example", inject((HammockConfig config, MockHttpBackend hb, ObjectStore s){
-      config.documentFormat = new JsonApiOrgFormat();
-      config.set({
-        "posts" : {
-          "type" : Post,
-          "route" : "custom_posts",
-          "serializer" : serializePost,
-          "deserializer" : deserializePost
-        }
-      });
+  describe("Different Types of Responses", () {
+    final post = new IntegrationPost()..id = 123..title = "new";
 
-      hb.whenGET("/custom_posts/123").respond({"posts" : [{"id" : 123, "title" : "title"}]});
-      wait(s.one(Post, 123), (post) {
-        expect(post.title).toEqual("title");
-      });
+    it("works when when a server returns an updated resource",
+      (HammockConfig config, MockHttpBackend hb, ObjectStore s) {
 
-      hb.expectPUT("/custom_posts/123", '{"posts":[{"id":123,"title":"new"}]}');
-      wait(s.save(new Post()..id = 123..title = "new"));
-    }));
+        config.set({
+            "posts" : {
+                "type" : IntegrationPost,
+                "serializer" : serializePost,
+                "deserializer" : deserializePost
+            }
+        });
+
+        hb.expectPUT("/posts/123").respond({"id" : 123, "title" : "updated"});
+
+        wait(s.save(post), (up) {
+          expect(up.title).toEqual("updated");
+        });
+
+        hb.expectPUT("/posts/123").respond(422, {"id" : 123, "title" : "updated", "errors" : "some errors"}, {});
+
+        waitForError(s.save(post), (up, s) {
+          expect(up.title).toEqual("updated");
+          expect(up.errors).toEqual("some errors");
+        });
+    });
+
+    it("works when a server returns a status",
+      (HammockConfig config, MockHttpBackend hb, ObjectStore s) {
+
+        config.set({
+            "posts" : {
+                "type" : IntegrationPost,
+                "serializer" : serializePost,
+                "deserializer" : {
+                  "command" : {
+                    "success" : (obj, r) => true,
+                    "error" : (obj, r) => r.content["errors"]
+                  }
+                }
+            }
+        });
+
+
+        hb.expectPUT("/posts/123").respond("OK");
+
+        wait(s.save(post), (res) {
+          expect(res).toBeTrue();
+        });
+
+        hb.expectPUT("/posts/123").respond(422, {"errors" : "some errors"}, {});
+
+        waitForError(s.save(post), (errors) {
+          expect(errors).toEqual("some errors");
+        });
+    });
   });
 }
 
 class JsonApiOrgFormat extends JsonDocumentFormat {
   resourceToJson(Resource res) =>
       {res.type.toString(): [res.content]};
-
-  manyResourcesToJson(List<Resource> list) =>
-      throw 'not needed';
 
   Resource jsonToResource(type, json) =>
       resource(type, json[type][0]["id"], json[type][0]);
